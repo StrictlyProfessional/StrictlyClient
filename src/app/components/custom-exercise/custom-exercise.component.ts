0f278e70e9ff3fd8ebaa184c6f6bcebc11a8ec92
Tyler Throop <tyler.throop@gmail.com>
2021-07-17 15:54:41 -0700

    adding calculating level to the exercises

--- src/app/components/custom-exercise/custom-exercise.component.ts
+++ src/app/components/custom-exercise/custom-exercise.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { eCard } from 'src/app/classes/cardinterfaces';
+import { HttpClient } from '@angular/common/http';
+import { User } from 'src/app/classes/classes';
+import { grabUser } from 'src/app/functions/userFunc';
 
 @Component({
   selector: 'app-custom-exercise',
@@ -27,7 +30,7 @@ export class CustomExerciseComponent implements OnInit {
   data: eCard = {
     state: "default"
   };
-
+  user : User = null;
   cardClicked() {
     if (this.data.state === "default") {
       this.data.state = "flipped";
@@ -35,11 +38,29 @@ export class CustomExerciseComponent implements OnInit {
       this.data.state = "default";
     }
   }
-  constructor() { }
+  constructor(
+    private httpClient : HttpClient
+  ) { }
 
   @Input() custExercise;
 
   ngOnInit(): void {
+    //this might need to be different for custom exercise.
+    //this is copying functionality from exercise
+    this.user = grabUser();
   }
-
+  calculateLevel(){
+    let experience = this.user.experience;
+    let newLevel = Math.floor((experience * 69)/420);
+    this.user.experience = newLevel;
+    this.httpClient.post('http://ec2-3-87-255-246.compute-1.amazonaws.com:8080/strictly/users/update',this.user)
+    .subscribe(
+      response =>{
+        console.log(response);
+      },
+      error =>{
+        alert("lol didnt work");
+      }
+    )
+  }
 }
